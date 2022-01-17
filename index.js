49eb155a3a6b19a7d9f2d8d13766f114c99b8430
Akiva Gordon <akivag29@gmail.com>
2022-01-16 22:02:39 -0500

    Add basic search

---
+++ index.js
@@ -0,0 +1,26 @@
+const items = ['Booster seat']
+
+document.addEventListener('DOMContentLoaded', () => {
+  const searchEl = document.querySelector('#search')
+  const itemsEl = document.querySelector('#items')
+
+  const renderItems = (query) => {
+    const foundItems = items.filter((item) =>
+      item.toLowerCase().startsWith(query)
+    )
+
+    if (!foundItems.length) {
+      itemsEl.innerHTML = `<p>No matches :(</p>`
+      return
+    }
+
+    itemsEl.innerHTML = foundItems.map((item) => `<li>${item}</li>`).join('')
+  }
+
+  searchEl.addEventListener('input', (e) => {
+    const query = e.target.value
+    renderItems(query)
+  })
+
+  renderItems('')
+})
